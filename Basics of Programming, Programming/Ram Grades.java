51468086af66b532aaaaf8a01a5c47dcb06c5b68
Yogesh swami <43330286+swamiyogesh1812@users.noreply.github.com>
2022-10-18 10:54:19 +0530

    Create Ram Grades.java

---
+++ Basics of Programming, Programming/Ram Grades.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+class Main
+{
+public static void main(String[] args)
+{ 
+    Scanner sc=new Scanner(System.in);
+    int a, b, c, d; 
+    a=sc.nextInt();
+    b=sc.nextInt();
+    c=sc.nextInt();
+    d=sc.nextInt();
+    int percentage= (a+b+c+d)/4;
+    System.out.println(percentage);
+
+   }
+}

877587e1368e55ae9e7ffaceb1aff8b8e5c05668
Yogesh swami <43330286+swamiyogesh1812@users.noreply.github.com>
2022-10-18 11:11:56 +0530

    Create Ice cream.java

---
+++ Ternary Operator, Loops - In Class/Ice cream.java
@@ -0,0 +1,14 @@
+static int Icecreams (int N, int D){
+    //Enter your code here
+    int rem=0;
+    int remaining =0;
+    for(int i=1; i<=D; i++){
+        int eaten=(N/2);
+        rem=(N-eaten);
+        remaining =(3*rem);
+        N=remaining;
+    }
+    return N;
+    
+    
+    }
